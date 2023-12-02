07d9b9969a639b533ddc91572585fd0b80034fac
thanhKasper <kieutienthanh207@gmail.com>
2023-12-02 21:35:40 +0700

    add fake data

---
+++ dataTest.js
@@ -0,0 +1,110 @@
+data = [
+    {
+        id: 1,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 2,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 3,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 4,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 5,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 6,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 7,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 8,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 9,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 10,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 11,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    },
+    {
+        id: 12,
+        name: "NVA",
+        age: 19,
+        gender: "Male",
+        phone: ["4789327178", "0192875924"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo, eros vel imperdiet mattis, lacus felis molestie augue, vel aliquam massa purus quis enim. In hac habitasse platea dictumst. Sed hendrerit velit sed ante semper fermentum. Nam ornare sodales volutpat. Morbi laoreet dui quis sem tempor, id eleifend quam posuere. Donec finibus odio mattis est pharetra lacinia in sit amet metus. Integer molestie quis leo et pellentesque. Nam consequat justo at malesuada interdum. Integer porta urna orci, sit amet pretium velit sodales quis. Ut suscipit blandit ipsum, nec blandit velit pulvinar pretium. Nam hendrerit ullamcorper justo quis finibus. Sed nunc massa, elementum quis orci efficitur, porta porta urna. Curabitur ac pulvinar orci.",
+        degree: ["Bachelor of Computer Science", "Comptia A+", "Web Developer"]
+    }
+]
