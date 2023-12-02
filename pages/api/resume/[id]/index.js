f1f928f45d3b3409a61ef2979a915c3204eb1779
HThanh-how <thanhlt1235@gmail.com>
2023-12-03 01:16:05 +0700

    join hết 4 bảng, còn bảng language

--- pages/api/resume/[id]/index.js
+++ pages/api/resume/[id]/index.js
@@ -2,6 +2,8 @@
 
 import resumedata from '../../database/resumes.json';
 import phonedata from '../../database/phones.json';
+import degreedata from '../../database/degrees.json';
+import userdata from '../../database/users.json';
 // Export a default function to handle api route
 export default async function handler(req, res) {
   // Get the id from the query
@@ -10,6 +12,7 @@ export default async function handler(req, res) {
 
   
   const resume = resumedata.find((item) => item.resume_id == id);
+  const user = userdata.find((item) => item.user_id == id);
   let resumeInfo = {
     resume_id: resume.resume_id,
     name: resume.name,
@@ -19,15 +22,27 @@ export default async function handler(req, res) {
     description: resume.description,
     phone_number: [],
     reg_date: resume.reg_date,
+    email: user.email,
+    degree: [],
   };
   let phoneNumbers = phonedata.filter(
     (phone) => phone.resume_id === resume.resume_id
   );
 
-
   for (let phone of phoneNumbers) {
     resumeInfo.phone_number.push(phone.phone_number);
   }
+
+  let degree = degreedata.filter(
+    (degreeID) => degreeID.resume_id === resume.resume_id
+  );
+
+  for (let degreeID of degree) {
+    resumeInfo.degree.push(degreeID.description);
+  }
+
+  
+
 
   res.json(resumeInfo);
 }
