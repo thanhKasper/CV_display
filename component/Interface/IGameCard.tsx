80d3be35c25fa1111bbc5f9029865599103762bd
HThanh-how <thanhlt1235@gmail.com>
2023-12-03 16:09:50 +0700

    update lại hoa thường

---
+++ component/Interface/IGameCard.tsx
@@ -0,0 +1,10 @@
+export interface Resume {
+    resume_id: number;
+    name: string;
+    age: string;
+    user_id: number;
+    gender: string;
+    description: string;
+    language: string;
+    reg_date: string;
+  }
